d811632c3ad38f6cab3fbb7e59d805897b1abd68
daniel gil <danielgilcan@gmail.com>
2025-01-25 14:03:38 +0100

    segunda carga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { WorkComponent } from './components/work/work.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent, ContactComponent, WorkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
